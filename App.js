f3c540c4db272ee5a7de0649fd5fa415fa5384f6
Aniket Chakraborty <139797826+otpidusmatar@users.noreply.github.com>
2023-10-28 17:44:30 -0500

    Added Bio Cell Transport and Energy Quizlet

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Launch from './Launch.jsx';
 import Home from './Home.jsx';
+import BCTE from './quizletpages/BCellTransportandEnergy.jsx';
+import TestQuizletB from './TestQuizletB.jsx';
+import TestQuizletC from './TestQuizletC.jsx';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +14,9 @@ function App() {
       <Stack.Navigator>
         <Stack.Screen name="Launch" component={Launch} />
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Cell Transport and Energy" component={BCTE} />
+        <Stack.Screen name="Quizlet 2" component={TestQuizletB} />
+        <Stack.Screen name="Quizlet 3" component={TestQuizletC} />
       </Stack.Navigator>
     </NavigationContainer>
   );
