f2840f0c6d79ad61321953dcdad05aa249360a73
Aniket Chakraborty <139797826+otpidusmatar@users.noreply.github.com>
2023-10-28 21:55:10 -0500

    Added Options Toolbar

---
+++ OptionsToolbar.jsx
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { Card } from 'react-native-paper';
+
+class OptionsToolbar extends Component {
+  render() {
+  return (
+    <View style={styles.buttonToolbarContainer}>
+        <Card>
+          <View style={styles.buttonContainer}>
+            <Pressable 
+              style={styles.button} 
+              onPress={() => alert('hi')}>
+              <Text style={styles.buttonLabel}>Content</Text>
+            </Pressable>
+          </View>
+        </Card>
+        <Card>
+          <View style={styles.buttonContainer}>
+            <Pressable 
+              style={styles.button} 
+              onPress={() => alert('hi')}>
+              <Text style={styles.buttonLabel}>Help Guide</Text>
+            </Pressable>
+          </View>
+        </Card>
+      </View>
+  )
+}
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 180,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+  },
+  buttonToolbarContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 8,
+    bottom: 0,
+    width: 420,
+    height: 100,
+    position: 'absolute',
+  },
+  button: {
+    borderRadius: 10,
+    width: '25%',
+    height: '10%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  buttonLabel: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    position: 'absolute',
+  },
+});
+
+export default OptionsToolbar;
