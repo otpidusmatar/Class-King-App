5ecda6430c604dfad36543e9c43e6288e6f36314
Aniket Chakraborty <139797826+otpidusmatar@users.noreply.github.com>
2023-10-29 08:40:53 -0500

    Revamped Launch Screen

--- Launch.jsx
+++ Launch.jsx
@@ -1,14 +1,16 @@
-import { Text, Pressable, SafeAreaView, StyleSheet, View } from 'react-native';
+import { Text, Pressable, SafeAreaView, StyleSheet, View, ImageBackground } from 'react-native';
 import { Card } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
+import BackgroundImage from './assets/learning-3245793_1280.jpg';
 
 export default function Launch() {
   const navigation = useNavigation();
 
   return (
     <SafeAreaView style={styles.container}>
+      <ImageBackground source={BackgroundImage} resizeMode="cover" style={styles.image}>
       <Text style={styles.paragraph}>Welcome to Class King</Text>
-      <Text style={styles.subheading}>Ready to start studying?</Text>
+      <Text style={styles.subheading}>Ready to start practicing?</Text>
       <Card>
         <View style={styles.buttonContainer}>
           <Pressable
@@ -18,6 +20,8 @@ export default function Launch() {
           </Pressable>
         </View>
       </Card>
+      <Text style={styles.subheading}>The Ultimate School Prep Guide</Text>
+      </ImageBackground>
     </SafeAreaView>
   )
 }
@@ -29,18 +33,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#ecf0f1',
     padding: 8,
   },
+  image: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
   paragraph: {
-    margin: 24,
+    margin: 20,
     marginBottom: 0,
     fontSize: 30,
     fontWeight: 'bold',
     textAlign: 'center',
+    textShadowColor: 'rgba(0, 0, 0, 1)',
+    textShadowRadius: 0,
+    textShadowOffset: {width: 4, height: 4},
+    color: 'white',
   },
   subheading: {
-    margin: 24,
+    margin: 17,
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
+    textShadowColor: 'rgba(0, 0, 0, 1)',
+    textShadowRadius: 0,
+    textShadowOffset: {width: 4, height: 4},
+    color: 'white',
   },
   buttonContainer: {
     width: 320,
