c72168d9d241c8f38080dfb1cce9fc817a5ab216
Aniket Chakraborty <139797826+otpidusmatar@users.noreply.github.com>
2023-10-30 19:15:57 -0500

    Updated Design

--- mappinglabs/EastAsia.jsx
+++ mappinglabs/EastAsia.jsx
@@ -1,6 +1,9 @@
-import { ScrollView, View, Text, Pressable, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, Pressable, StyleSheet, SafeAreaView, ImageBackground } from 'react-native';
 import { Component } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import OptionsToolbar from '../OptionsToolbar.jsx';
+import BackgroundImage from '../assets/learning-3245793_1280.jpg';
+import RedPanelImage from '../assets/kw-red-rectangle-button-panel-hi.png';
 
 class MapList extends Component {
   state = {
@@ -16,11 +19,13 @@ class MapList extends Component {
                {
                   this.state.names.map((item, index) => (
                      <View key = {item.id} style = {styles.item}>
+                     <ImageBackground source={RedPanelImage} resizeMode='cover' style={styles.image}>
                       <Pressable 
                         style={styles.buttonForList} 
                         onPress={() => this.props.navigation.navigate(item.name)}>
                         <Text style={styles.buttonLabel}>{item.name}</Text>
                       </Pressable>
+                      </ImageBackground>
                      </View>
                   ))
                }
@@ -34,11 +39,34 @@ export default function EastAsia() {
   const navigation = useNavigation();
   
   return (
-    <MapList navigation={navigation} />
+    <SafeAreaView style={styles.container}>
+      <ImageBackground source={BackgroundImage} resizeMode={'cover'} style={styles.container}>
+        <MapList navigation={navigation} />
+        <OptionsToolbar navigation={navigation} />
+      </ImageBackground>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'top',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },  
+  image: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 30,
+      margin: 2,
+      width: 380,
+      borderColor: '#2a4944',
+      borderWidth: 1,
+      backgroundColor: 'white',
+   },
   buttonContainer: {
     width: 70,
     height: 68,
@@ -68,15 +96,16 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     position: 'absolute',
+    color: 'white',
+    textShadowColor: 'black',
+    textShadowRadius: 1,
+    textShadowOffset: {width: 2, height: 2}
   },
   item: {
       flexDirection: 'row',
       justifyContent: 'center',
       alignItems: 'center',
-      padding: 30,
-      margin: 2,
-      borderColor: '#2a4944',
-      borderWidth: 1,
-      backgroundColor: 'white',
+      padding: 0,
+      margin: 0,
    },
 });
